834d06b38e7de81a77ea17fcbc1d4815eb89f976
bjorn <tie.372@gmail.com>
2017-01-21 04:08:52 -0800

    Initial commit;

---
+++ README.md
@@ -0,0 +1,191 @@
+maf
+===
+
+A small 3D math library for Lua.
+
+Example
+---
+
+```lua
+local maf = require 'maf'
+
+local a = maf.vector(1, 2, 3)
+local b = maf.vector(4, 5, 6)
+
+local c = a + b
+local d = maf.rotation():between(a, b)
+
+print(c:unpack())
+print(d:getAngleAxis())
+```
+
+Documentation
+---
+
+In general, any function that returns a vector or a rotation will write its result into the last
+parameter, `out`.  The function also returns `out` for convenience, to allow for method chaining.
+If `out` is unspecified, then the result will be written into the first parameter instead.
+
+### Vectors
+
+##### `maf.vector(x, y, z)` or `maf.vec3(x, y, z)`
+
+Create a new vector.  Omitted elements will be set to zero.
+
+The `x`, `y`, and `z` keys contain the individual components of the vector.
+
+The following metamethods are defined for vectors:
+
+- `v + v` - Add two vectors.
+- `v - v` - Subtract two vectors.
+- `v * v` - Multiply two vectors.
+- `v * n` - Scale a vector by a number.
+- `v / v` - Divide two vectors.
+- `-v` - Negate a vector.
+- `#v` - Get the length of a vector.
+
+##### `vector:unpack()`
+
+Get the individual components of the vector.
+
+##### `vector:add(v2, [out])`
+
+Add two vectors.
+
+##### `vector:sub(v2, [out])`
+
+Subtract two vectors.
+
+##### `vector:mul(v2, [out])`
+
+Multiply two vectors.
+
+##### `vector:div(v2, [out])`
+
+Divide two vectors.
+
+##### `vector:scale(s, [out])`
+
+Multiply each component of the vector by a number `s`.
+
+##### `vector:length()`
+
+Return the length of the vector.
+
+##### `vector:normalize([out])`
+
+Set the length of a vector to 1.
+
+##### `vector:distance(v2)`
+
+Return the distance between two vectors.
+
+##### `vector:angle(v2)`
+
+Return the angle between two vectors, in radians.
+
+##### `vector:dot(v2)`
+
+Return the dot product of two vectors, defined as `v1.x * v2.x + v1.y * v2.y + v1.z * v2.z`.
+
+##### `vector:cross(v2, [out])`
+
+Return the cross product of two vectors.  The cross product is a vector that is perpendicular to the
+two input vectors.
+
+##### `vector:lerp(v2, t, [out])`
+
+Mixes two vectors together using linear interpolation.  The parameter `t` controls how they are
+mixed (0 will return `vector`, 1 will return `v2`, .5 will return a 50/50 blend of the two vectors,
+etc.).
+
+##### `vector:rotate(r, [out])`
+
+Applies a rotation `r` to the vector.
+
+### Rotations
+
+##### `maf.rotation(x, y, z, w)` or `maf.quat(x, y, z, w)`
+
+Creates a new rotation (quaternion) from x, y, z, w components.  Unless you are some sort of wizard
+that understands quaternions, it's probably easier to call `:angleAxis` or `:between` on the
+rotation to initialize it.
+
+The following metamethods are defined for rotations:
+
+- `q + q` - Add two rotations.
+- `q - q` - Subtract two rotations.
+- `q * q` - Multiply two rotations, resulting in a rotation that is equivalent to applying the first
+  rotation followed by the second.
+- `q * v` - Multiply a rotation by a vector, returning the rotated vector.
+- `-q` - Negate a rotation.
+- `#q` - Get the length of a rotation.
+
+##### `rotation:unpack()`
+
+Get the individual components of the rotation.
+
+##### `angle, x, y, z = rotation:getAngleAxis()`
+
+Get the angle/axis representation of the rotation.
+
+##### `rotation:angleAxis(angle, vector)` or `rotation:angleAxis(angle, x, y, z)`
+
+Set the rotation using angle/axis representation.
+
+##### `rotation:between(v1, v2)`
+
+Set the rotation defined by rotating from `v1` to `v2`.
+
+##### `rotation:add(r2, [out])`
+
+Add two rotations together.  To combine two rotations together, multiply them instead.
+
+##### `rotation:sub(r2, [out])`
+
+Subtract two rotations.
+
+##### `rotation:mul(r2, [out])`
+
+Multiply two rotations together, returning a rotation that combines the two.
+
+##### `rotation:scale(s, [out])`
+
+Multiply each component of the rotation by a number `s`.
+
+##### `rotation:length()`
+
+Return the length of the rotation.
+
+##### `rotation:normalize([out])`
+
+Normalize a rotation, setting its length to 1.
+
+##### `rotation:lerp(r2, t, [out])`
+
+Mix two rotations together using linear interpolation.  This is faster than `:slerp` but less
+accurate.
+
+##### `rotation:slerp(r2, t, [out])`
+
+Mix two rotations together using spherical linear interpolation.
+
+Garbage Collection Is Scary
+---
+
+Keep in mind that each time a new vector or rotation is created, a small memory allocation is made.
+Normally you don't need to worry about this, since Lua has a garbage collector that will clean up
+after you.  However, allocating a large number of objects can put stress on the garbage collector,
+which can lead to performance problems.  This can happen more often that you'd think if you're, for
+example, creating new vectors in the update loop of a game.
+
+It gets worse.  Although it's convenient to be able to write `a + b` to add vectors together, a new
+vector must be allocated to store the result.  This makes it really easy to allocate a bunch of
+vectors without even knowing it!  To avoid this sneaky performance trap, you can add two vectors
+using `a:add(b)`, which will reuse `a` to store the result.  Another strategy is to allocate a set
+of reusable temporary vectors and use `a:add(b, tmp)`.
+
+License
+---
+
+MIT, see [`LICENSE`](LICENSE) for details.
---
+++ maf.lua
@@ -0,0 +1,296 @@
+-- maf
+-- https://github.com/bjornbytes/maf
+-- MIT License
+
+local ffi = type(jit) == 'table' and jit.status() and require 'ffi'
+local vec3, quat
+
+local vtmp1
+local vtmp2
+local qtmp1
+
+vec3 = {
+  __call = function(_, x, y, z)
+    return setmetatable({ x = x or 0, y = y or 0, z = z or 0 }, vec3)
+  end,
+
+  __add = function(v, u) return v:add(u, vec3()) end,
+  __sub = function(v, u) return v:sub(u, vec3()) end,
+  __mul = function(v, u)
+    if vec3.isvec3(u) then return v:mul(u, vec3())
+    elseif type(u) == 'number' then return v:scale(u, vec3())
+    else error('vec3s can only be multiplied by vec3s and numbers') end
+  end,
+  __div = function(v, u) return v:div(u, vec3()) end,
+  __unm = function(v) return v:scale(-1) end,
+  __len = function(v) return v:length() end,
+
+  __index = {
+    isvec3 = function(x)
+      return ffi and ffi.istype('vec3', x) or getmetatable(x) == vec3
+    end,
+
+    unpack = function(v)
+      return v.x, v.y, v.z
+    end,
+
+    add = function(v, u, out)
+      out = out or v
+      out.x = v.x + u.x
+      out.y = v.y + u.y
+      out.z = v.z + u.z
+      return out
+    end,
+
+    sub = function(v, u, out)
+      out = out or v
+      out.x = v.x - u.x
+      out.y = v.y - u.y
+      out.z = v.z - u.z
+      return out
+    end,
+
+    mul = function(v, u, out)
+      out = out or v
+      out.x = v.x * u.x
+      out.y = v.y * u.y
+      out.z = v.z * u.z
+      return out
+    end,
+
+    div = function(v, u, out)
+      out = out or v
+      out.x = v.x / u.x
+      out.y = v.y / u.y
+      out.z = v.z / u.z
+      return out
+    end,
+
+    scale = function(v, s, out)
+      out = out or v
+      out.x = v.x * s
+      out.y = v.y * s
+      out.z = v.z * s
+      return out
+    end,
+
+    length = function(v)
+      return math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z)
+    end,
+
+    normalize = function(v, out)
+      out = out or v
+      local len = v:length()
+      return len == 0 and v or v:scale(1 / len, out)
+    end,
+
+    distance = function(v, u)
+      return vec3.sub(v, u, vtmp1):length()
+    end,
+
+    angle = function(v, u)
+      return math.acos(v:dot(u) / (v:length() + u:length()))
+    end,
+
+    dot = function(v, u)
+      return v.x * u.x + v.y * u.y + v.z * u.z
+    end,
+
+    cross = function(v, u, out)
+      out = out or v
+      out.x = v.y * u.z - v.z * u.y
+      out.y = v.z * u.x - v.x * u.z
+      out.z = v.x * u.y - v.y * u.x
+      return out
+    end,
+
+    lerp = function(v, u, t, out)
+      out = out or v
+      out.x = v.x + (u.x - v.x) * t
+      out.y = v.y + (u.y - v.y) * t
+      out.z = v.z + (u.z - v.z) * t
+      return out
+    end,
+
+    rotate = function(v, q, out)
+      out = out or v
+      local u, c, o = vtmp1, vtmp2, out
+      u.x, u.y, u.z = q.x, q.y, q.z
+      o.x, o.y, o.z = v.x, v.y, v.z
+      u:cross(v, c)
+      local uu = u:dot(u)
+      local uv = u:dot(v)
+      o:scale(q.w * q.w - uu)
+      u:scale(2 * uv)
+      c:scale(2 * q.w)
+      return o:add(u:add(c))
+    end
+  }
+}
+
+quat = {
+  __call = function(_, angle, x, y, z)
+    return setmetatable({ x = x, y = y, z = z, w = w }, quat)
+  end,
+
+  __add = function(q, r) return q:add(r, quat()) end,
+  __sub = function(q, r) return q:sub(r, quat()) end,
+  __mul = function(q, r)
+    if quat.isquat(r) then return q:mul(r, quat())
+    elseif vec3.isvec3(r) then return r:rotate(q, vec3())
+    else error('quats can only be multiplied by quats and vec3s') end
+  end,
+  __unm = function(q) return q:scale(-1) end,
+  __len = function(q) return q:length() end,
+
+  __index = {
+    isquat = function(x)
+      return ffi and ffi.istype('quat', x) or getmetatable(x) == quat
+    end,
+
+    unpack = function(q)
+      return q.x, q.y, q.z, q.w
+    end,
+
+    getAngleAxis = function(q)
+      if q.w > 1 or q.w < -1 then q:normalize() end
+      local s = math.sqrt(1 - q.w * q.w)
+      s = s < .0001 and 1 or 1 / s
+      return 2 * math.acos(q.w), q.x * s, q.y * s, q.z * s
+    end,
+
+    -- Set rotation from angle/axis representation
+    angleAxis = function(q, angle, x, y, z)
+      if getmetatable(x) == vec3 then x, y, z = x.x, x.y, x.z end
+      local s = math.sin(angle * .5)
+      local c = math.cos(angle * .5)
+      q.x = x * s
+      q.y = y * s
+      q.z = z * s
+      q.w = c
+      return q
+    end,
+
+    -- Set rotation from one vector to another
+    between = function(q, u, v)
+      local dot = u:dot(v)
+      if dot > .99999 then
+        q.x, q.y, q.z, q.w = 0, 0, 0, 1
+        return q
+      elseif dot < -.99999 then
+        vtmp1.x, vtmp1.y, vtmp1.z = 1, 0, 0
+        vtmp1:cross(u)
+        if #vtmp1 < .00001 then
+          vtmp1.x, vtmp1.y, vtmp1.y = 0, 1, 0
+          vtmp1:cross(u)
+        end
+        vtmp1:normalize()
+        return q:angleAxis(math.pi, vtmp1)
+      end
+
+      q.x, q.y, q.z = u.x, u.y, u.z
+      vec3.cross(q, v)
+      q.w = 1 + dot
+      return q:normalize()
+    end,
+
+    add = function(q, r, out)
+      out = out or q
+      out.x = q.x + r.x
+      out.y = q.y + r.y
+      out.z = q.z + r.z
+      out.w = q.w + r.w
+      return out
+    end,
+
+    sub = function(q, r, out)
+      out = out or q
+      out.x = q.x - r.x
+      out.y = q.y - r.y
+      out.z = q.z - r.z
+      out.w = q.w - r.w
+      return out
+    end,
+
+    mul = function(q, r, out)
+      out = out or q
+      local qx, qy, qz, qw = q:unpack()
+      local rx, ry, rz, rw = q:unpack()
+      out.x = qx * rw + qw * rx - qy * rz - qz * ry
+      out.y = qy * rw + qw * ry - qz * rx - qx * rz
+      out.z = qz * rw + qw * rz - qx * ry - qy * rx
+      out.w = qw * rw - qx * rx - qy * ry - qz * rz
+      return out
+    end,
+
+    scale = function(q, s, out)
+      out = out or q
+      out.x = q.x * s
+      out.y = q.y * s
+      out.z = q.z * s
+      out.w = q.w * s
+      return out
+    end,
+
+    length = function(q)
+      return math.sqrt(q.x * q.x + q.y * q.y + q.z * q.z + q.w * q.w)
+    end,
+
+    normalize = function(q, out)
+      out = out or q
+      local len = q:length()
+      return len == 0 and q or q:scale(1 / len, out)
+    end,
+
+    lerp = function(q, r, t, out)
+      out = out or q
+      r:scale(t, qtmp1)
+      q:scale(1 - t, out)
+      return out:add(qtmp1)
+    end,
+
+    slerp = function(q, r, t, out)
+      out = out or q
+
+      local dot = q.x * r.x + q.y * r.y + q.z * r.z + q.w * r.w
+      if dot < 0 then
+        dot = -dot
+        r:scale(-1)
+      end
+
+      if 1 - dot < .0001 then
+        return q:lerp(r, t, out)
+      end
+
+      local theta = math.acos(dot)
+      q:scale(math.sin((1 - t) * theta), out)
+      r:scale(math.sin(t * theta), qtmp1)
+      return out:add(qtmp1):scale(1 / math.sin(theta))
+    end
+  }
+}
+
+if ffi then
+  ffi.cdef [[
+    typedef struct { float x, y, z; } vec3;
+    typedef struct { float x, y, z, w; } quat;
+  ]]
+
+  vec3 = ffi.metatype('vec3', vec3)
+  quat = ffi.metatype('quat', quat)
+else
+  setmetatable(vec3, vec3)
+  setmetatable(quat, quat)
+end
+
+vtmp1 = vec3()
+vtmp2 = vec3()
+qtmp1 = quat()
+
+return {
+  vec3 = vec3,
+  quat = quat,
+
+  vector = vec3,
+  rotation = quat
+}
